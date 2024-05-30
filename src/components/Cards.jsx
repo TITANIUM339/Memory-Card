b26eea66cae6b932cb0cd6e90fd15ab4b2b18ae8
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-05-30 13:32:42 +0300

    Flip and shuffle cards on click

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,72 @@
 import Card from "./Card.jsx";
 import "../styles/Cards.css";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+// https://stackoverflow.com/a/2450976
+function shuffle(unShuffledArray) {
+    const array = JSON.parse(JSON.stringify(unShuffledArray));
+
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
+
+    return array;
+}
 
 function Cards({ cards, updateScore }) {
     const [clickedCards, setClickedCards] = useState([]);
+    const [shuffledCards, setShuffledCards] = useState(cards);
+
+    useEffect(() => {
+        const flipToBackKeyframes = [
+            { pointerEvents: "none" },
+            { transform: "rotateY(180deg)", pointerEvents: "none" },
+        ];
+
+        const flipToBackOptions = {
+            duration: 750,
+            iterations: 1,
+            fill: "forwards"
+        };
+
+        const flipToFrontKeyframes = [
+            { transform: "rotateY(0deg)", pointerEvents: "all" },
+        ];
+
+        const flipToFrontOptions = {
+            duration: 750,
+            iterations: 1,
+            fill: "forwards",
+        };
+
+        const cardElements = document.querySelectorAll(".card");
+
+        cardElements.forEach((card) => {
+            card.animate(flipToBackKeyframes, flipToBackOptions).finished.then(() => {
+                card.animate(flipToFrontKeyframes, flipToFrontOptions);
+                setShuffledCards(shuffle(shuffledCards));
+            });
+        });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [clickedCards]);
     
     return (
         <div className="cards">
-            {cards.map((card) => (
+            {shuffledCards.map((card) => (
                 <Card
                     key={card.id}
                     imgUrl={card.imgUrl}
